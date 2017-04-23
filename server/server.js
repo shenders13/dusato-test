00e57c22d55f74ccf6acfee700ad4c2cc0d736f6
Sam Henderson <hendersonsam456@gmail.com>
2017-04-23 20:21:42 +1000

    heroku

--- server/server.js
+++ server/server.js
@@ -39,7 +39,8 @@ app.post('/submission', serverHelpers.createSubmission);
 // });
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/dusato');
+const mongoURL = process.env.MONGODB_URI || 'mongodb://localhost/dusato'
+mongoose.connect(mongoURL);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
