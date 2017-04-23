054ca11fe7d158b44c284f0a826dbf751dc2f645
Sam Henderson <hendersonsam456@gmail.com>
2017-04-23 22:14:31 +1000

    commit before deploy

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const Submission = require('../db/submissionModel.js');
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
