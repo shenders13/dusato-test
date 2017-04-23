ae98eae7cb4aced70d1dd336bc635a0f7e1cc9a5
Sam Henderson <hendersonsam456@gmail.com>
2017-04-23 22:07:03 +1000

    commit before deploy

--- server/server.js
+++ server/server.js
@@ -18,11 +18,19 @@ const serverHelpers = require('./serverHelpers')
 const app = express();
 const Submission = require('../db/submissionModel.js');
 
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+// app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+// });
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
